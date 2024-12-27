15954cd2689c9ec6b9afa32740c97ab652e8eb38
Cristian.Alexa <cristian.alexa@vectron.de>
2024-12-27 14:18:42 +0200

    * Added exception handling

---
+++ src/main/java/com/playground/budgeting/api/error/ErrorHandler.java
@@ -0,0 +1,21 @@
+package com.playground.budgeting.api.error;
+
+import graphql.GraphQLError;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public GraphQLError handleInternalServerError(final RuntimeException runtimeException) {
+        log.debug("Error caught: {}", runtimeException.getMessage());
+        return GraphQLError.newError()
+            .message("Internal Server Error")
+            .errorType(ErrorType.INTERNAL_ERROR)
+            .build();
+    }
+}
